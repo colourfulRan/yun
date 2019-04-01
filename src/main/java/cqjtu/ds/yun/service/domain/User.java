a6d2293930c1dc7392dc99f8be49a7011a41d0f7
zr <2351161775@qq.com>
2019-04-01 20:14:46 +0800

    文件上传基本实现

--- src/main/java/cqjtu/ds/yun/service/domain/User.java
+++ src/main/java/cqjtu/ds/yun/service/domain/User.java
@@ -1,9 +1,14 @@
 package cqjtu.ds.yun.service.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="user")
+@Getter
+@Setter
 public class User {
 
     @Id
