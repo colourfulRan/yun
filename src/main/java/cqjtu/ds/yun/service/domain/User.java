d4401d58872f061bee1885f4a52dfaa1ba61718b
hxren <1546175764@qq.com>
2019-05-25 16:35:42 +0800

    5.25

--- src/main/java/cqjtu/ds/yun/service/domain/User.java
+++ src/main/java/cqjtu/ds/yun/service/domain/User.java
@@ -1,12 +1,13 @@
 package cqjtu.ds.yun.service.domain;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashMap;
 
 @Entity
 @Table(name="user")
@@ -22,12 +23,12 @@ public class User implements Serializable {
     private Integer userId;
     private String username;
     private String password;
+    private  String  sex;
+    private java.sql.Date brithday;
+    private String bloodType;
+    private  String brithPlace;
+    private  String livePlace;
     private String photo;
-    private Date birthday;
-    private String sex;
-    private String blood_type;
-    private String birth_place;
-    private String live_place;
 
 
     public Integer getUserId() {
@@ -61,4 +62,47 @@ public class User implements Serializable {
     public void setPhoto(String photo) {
         this.photo = photo;
     }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public java.sql.Date getBrithday() {
+        return brithday;
+    }
+
+    public void setBrithday(java.sql.Date brithday) {
+        this.brithday = brithday;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getBrithPlace() {
+        return brithPlace;
+    }
+
+    public void setBrithPlace(String brithPlace) {
+        this.brithPlace = brithPlace;
+    }
+
+    public String getLivePlace() {
+        return livePlace;
+    }
+
+    public void setLivePlace(String livePlace) {
+        this.livePlace = livePlace;
+    }
+
+
+
 }
